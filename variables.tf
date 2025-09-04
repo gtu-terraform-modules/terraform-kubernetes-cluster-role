694c2d4ff7781047725f4f69581ee9c9a9f5631d
Tim Khasanov <irugasan@gmail.com>
2025-09-04 15:58:46 +0700

    minor changes

--- variables.tf
+++ variables.tf
@@ -3,20 +3,6 @@ variable "service_account_name" {
   type        = string
 }
 
-variable "cluster_role_name" {
-  description = "The name of the ClusterRole to create"
-  type        = string
-}
-
-variable "cluster_role_permissions" {
-  description = "The permissions for the ClusterRole"
-  type        = list(object({
-    api_groups = list(string)
-    resources  = list(string)
-    verbs      = list(string)
-  }))
-}
-
 variable "namespace" {
   description = "The namespace in which to create the resources"
   type        = string
@@ -28,7 +14,21 @@ variable "namespace" {
   }
 }
 
+variable "cluster_role_name" {
+  description = "The name of the ClusterRole to create"
+  type        = string
+}
+
 variable "cluster_role_binding_name" {
   description = "The name of the ClusterRoleBinding to create"
   type        = string
+}
+
+variable "cluster_role_permissions" {
+  description = "The permissions for the ClusterRole"
+  type        = list(object({
+    api_groups = list(string)
+    resources  = list(string)
+    verbs      = list(string)
+  }))
 }
