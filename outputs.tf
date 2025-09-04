1ac784cec1ef74428c21a651e3c149e65d2fcfc4
Tim Khasanov <irugasan@gmail.com>
2025-09-04 17:48:58 +0700

    add output service_account_token

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "service_account_name" {
   value = kubernetes_service_account.this.metadata[0].name
 }
 
-output "cluster_role_name" {
-  value = kubernetes_cluster_role.this.metadata[0].name
-}
-
-output "secret_name" {
-  value = kubernetes_secret_v1.this.metadata[0].name
+output "service_account_token" {
+  value = kubernetes_secret_v1.this.data["token"]
 }
