229e74b3523126a91de32b80e5745fc55d10e181
Tim Khasanov <irugasan@gmail.com>
2025-09-04 14:36:54 +0700

    add secret_name outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "service_account_name" {
 output "cluster_role_name" {
   value = kubernetes_cluster_role.this.metadata[0].name
 }
+
+output "secret_name" {
+  value = kubernetes_secret_v1.this.metadata[0].name
+}
